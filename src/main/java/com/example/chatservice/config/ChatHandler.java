39e4cd4eded5978786d97e100d0402fe435dacfe
kimsh2948 <shkim2948@gmail.com>
2023-03-20 21:45:39 +0900

    feat: ChatHandler 기능 구현

--- src/main/java/com/example/chatservice/config/ChatHandler.java
+++ src/main/java/com/example/chatservice/config/ChatHandler.java
@@ -4,18 +4,28 @@ import com.example.chatservice.domain.ChatMessage;
 import com.example.chatservice.repository.ChatMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
-public class ChatHandler {
+public class ChatHandler implements WebSocketHandler {
 
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
+    @Override
     public Mono<Void> handle(WebSocketSession webSocketSession) {
-        return webSocketSession.send()
+        Flux<ChatMessage> chatMessageFlux = this.chatMessageRepository.findWithTailableCursorBy();
+
+        Flux<String> messageFlux = webSocketSession.receive()
+                .map(WebSocketMessage::getPayloadAsText);
+
+        return webSocketSession.send(
+                chatMessageFlux.mergeWith(messageFlux).map(webSocketSession::textMessage);
+        );
     }
 
 }
--- src/main/java/com/example/chatservice/repository/ChatMessageRepository.java
+++ src/main/java/com/example/chatservice/repository/ChatMessageRepository.java
@@ -2,7 +2,11 @@ package com.example.chatservice.repository;
 
 import com.example.chatservice.domain.ChatMessage;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
 
 public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, String> {
 
+    @Tailable
+    Flux<ChatMessage> findWithTailableCursorBy();
 }
