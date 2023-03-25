d8ce4c0a26586dd8fa3b9788a6cf9605beae4359
kimsh2948 <shkim2948@gmail.com>
2023-03-25 16:09:58 +0900

    feat: handlermapping으로 chathandler mapping 구현

--- src/main/java/com/example/chatservice/config/ChatHandler.java
+++ src/main/java/com/example/chatservice/config/ChatHandler.java
@@ -2,7 +2,8 @@ package com.example.chatservice.config;
 
 import com.example.chatservice.domain.ChatMessage;
 import com.example.chatservice.repository.ChatMessageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -11,20 +12,22 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class ChatHandler implements WebSocketHandler {
 
-    @Autowired
-    private ChatMessageRepository chatMessageRepository;
+    private final ChatMessageRepository chatMessageRepository;
 
     @Override
     public Mono<Void> handle(WebSocketSession webSocketSession) {
+        System.out.println("open");
         Flux<ChatMessage> chatMessageFlux = this.chatMessageRepository.findWithTailableCursorBy();
 
         Flux<String> messageFlux = webSocketSession.receive()
                 .map(WebSocketMessage::getPayloadAsText);
 
         return webSocketSession.send(
-                chatMessageFlux.mergeWith(messageFlux).map(webSocketSession::textMessage);
+                chatMessageFlux.merge(messageFlux).map(webSocketSession::textMessage)
         );
     }
 
---
+++ src/main/java/com/example/chatservice/config/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.example.chatservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketConfig {
+
+    private final ChatHandler chatHandler;
+
+    @Bean
+    public HandlerMapping webSocketHandlerMapping() {
+        Map<String, WebSocketHandler> map = new HashMap<>();
+        map.put("/chat", chatHandler);
+        System.out.println("open2");
+
+        SimpleUrlHandlerMapping handlerMapping =  new SimpleUrlHandlerMapping();
+        handlerMapping.setOrder(1);
+        handlerMapping.setUrlMap(map);
+        return handlerMapping;
+    }
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+}
